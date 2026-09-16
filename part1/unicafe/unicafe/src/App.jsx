e3acc7c07716164bd0ab55a110be8595a51f6c00
iromanrobert <hello@romanrobert.com>
2026-09-16 17:34:55 +0300

    FSO exercise 1.8 - 1.9

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
-const Button = ({onClick, text}) => {
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  percentage,
+  averageScore,
+}) => {
   return (
-    <button onClick={onClick}>{text}</button>
-  )
-}
+    <ul>
+      <li>good: {good}</li>
+      <li>neutral: {neutral}</li>
+      <li>bad: {bad}</li>
+      <li>all: {total}</li>
+      <li>percentage: {percentage}%</li>
+      <li>average: {total === 0 ? 0 : averageScore / total}</li>
+    </ul>
+  );
+};
 
 function App() {
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -17,50 +34,41 @@ function App() {
   const [percentage, setPercentage] = useState(0);
   const [averageScore, setAverageScore] = useState(0);
 
-  console.log(averageScore)
+  console.log(averageScore);
 
   const calculatePercentage = () => {
-
     let percentage;
     if (total === 0) {
-      return 0
+      return 0;
     }
-    percentage = good / total * 100
+    percentage = (good / total) * 100;
 
-    return  percentage.toFixed(1) }
+    return percentage.toFixed(1);
+  };
 
-  
-  
-  
   const handleGoodFeedback = () => {
-    const updateGood = good + 1
-    setGood(updateGood)
-    setTotal(updateGood + bad + neutral)
-    setPercentage(calculatePercentage())
-    setAverageScore(averageScore + 1)
+    const updateGood = good + 1;
+    setGood(updateGood);
+    setTotal(updateGood + bad + neutral);
+    setPercentage(calculatePercentage());
+    setAverageScore(averageScore + 1);
+  };
 
-  }
-  
   const handleNeutralFeedback = () => {
     const updateNeutral = neutral + 1;
-    setNeutral(updateNeutral)
-    setTotal(updateNeutral + bad + good)
-    setPercentage(calculatePercentage())
-    setAverageScore(averageScore)
-    
-    
-  }
+    setNeutral(updateNeutral);
+    setTotal(updateNeutral + bad + good);
+    setPercentage(calculatePercentage());
+    setAverageScore(averageScore);
+  };
 
   const handleBadFeedback = () => {
-    const updateBad = bad + 1
+    const updateBad = bad + 1;
     setBad(updateBad);
-    setTotal(updateBad + neutral + good)
-    setPercentage(calculatePercentage())
-    setAverageScore(averageScore - 1)
-    
-  }
-
-  
+    setTotal(updateBad + neutral + good);
+    setPercentage(calculatePercentage());
+    setAverageScore(averageScore - 1);
+  };
 
   return (
     <section>
@@ -72,17 +80,21 @@ function App() {
       </div>
       <div>
         <p>Statistics go here</p>
-        <ul>
-          <li>good: {good}</li>
-          <li>neutral: {neutral}</li>
-          <li>bad: {bad}</li>
-          <li>all: {total}</li>
-          <li>percentage: {percentage}%</li>
-          <li>average: {total === 0 ? 0 : averageScore / total}</li>
-        </ul>
+        {total === 0 ? (
+          <p>No feedback given</p>
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percentage={percentage}
+            averageScore={averageScore}
+          />
+        )}
       </div>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
